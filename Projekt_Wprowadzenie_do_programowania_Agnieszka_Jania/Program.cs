7bf89ca684e1bbc2c0c20b0278520ade99f89b07
agnieszka <agnieszka.jania1995@gmail.com>
2020-05-29 12:42:02 +0200

    Rozpoczynam prace

---
+++ Projekt_Wprowadzenie_do_programowania_Agnieszka_Jania/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projekt_Wprowadzenie_do_programowania_Agnieszka_Jania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
