48d7c4ec9fe40ddd7beb126e645c23b29e9f4899
agnieszka <agnieszka.jania1995@gmail.com>
2020-05-29 12:21:07 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Half_of_the_half
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int przypadki = int.Parse(Console.ReadLine());
+
+            for (int x  = 0; x < przypadki; x++)
+            {
+                string napis = Console.ReadLine();
+                char[] litery = napis.ToCharArray();
+                for(int i = 0; i < (litery.Length) / 2; i++)
+                {
+                    if( i % 2 == 0)
+                    {
+                        Console.Write(litery[i]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
